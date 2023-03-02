19ae90b886a564804a6f11287853d49ddba5d15f
John Shaffer <jdsha@proton.me>
2023-03-02 14:13:40 -0600

    Use srvc 0.15.0

--- flake.nix
+++ flake.nix
@@ -8,13 +8,20 @@
       url = "github:edolstra/flake-compat";
       flake = false;
     };
+    srvc = {
+      url = "github:insilica/rs-srvc/master";
+      inputs.flake-compat.follows = "flake-compat";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, srvc, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = import nixpkgs { inherit system; };
       in with pkgs; {
-        devShells.default = mkShell { buildInputs = [ srvc ]; };
+        devShells.default =
+          mkShell { buildInputs = [ srvc.packages.${system}.default ]; };
       });
 
 }
