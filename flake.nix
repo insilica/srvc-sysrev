ef3d9e9ef238ed37f58b158adecf25a8427e2bbd
John Shaffer <jdsha@proton.me>
2023-03-02 12:01:48 -0600

    Convert dev shell to nix flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "srvc sysrev project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in with pkgs; {
+        devShells.default = mkShell { buildInputs = [ srvc ]; };
+      });
+
+}
--- shell.nix
+++ shell.nix
@@ -1,18 +1,6 @@
-{ sources ? import ./nix/sources.nix, pkgs ? import sources.nixpkgs { } }:
-let
-  srvc = pkgs.rustPlatform.buildRustPackage rec {
-    pname = "srvc";
-    version = "3599eedbdc403ef25b01a73f1c4dcd5e994fd4ba";
-
-    src = pkgs.fetchFromGitHub {
-      owner = "insilica";
-      repo = "rs-srvc";
-      rev = "${version}";
-      sha256 = "sha256-BBotqS/w2aXACOWFuHfInmKne3Tn+LiH7o6oet1TR9A=";
-    };
-
-    cargoSha256 = "sha256-eZIJCVioXUjPnwXAAiR/hGmwsOjqCo7llbMMGRe2rl4=";
-
-    nativeBuildInputs = [ pkgs.webfs ];
-  };
-in with pkgs; mkShell { buildInputs = [ srvc ]; }
+(import (let lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+in fetchTarball {
+  url =
+    "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+  sha256 = lock.nodes.flake-compat.locked.narHash;
+}) { src = ./.; }).shellNix
